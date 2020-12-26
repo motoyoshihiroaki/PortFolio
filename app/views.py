37bff883b7359aae539ef4817621c9d722641bd0
M_Hiroaki <motoyoshihiroaki@gmail.com>
2020-12-26 15:58:24 +0900

    settingファイルを新規作成
    メールの送信処理などを作成した後に、開発用と本番用のsettingsファイルを作成した

--- app/views.py
+++ app/views.py
@@ -1,8 +1,22 @@
 from django.shortcuts import render
+import logging
+from django.urls import reverse_lazy
 
 # Create your views here.
 from django.views import generic
+from .forms import InquiryForm
+
+logger = logging.getLogger(__name__)
 
 class IndexView(generic.TemplateView):
     template_name = "index.html"
 
+class InquiryView(generic.FormView):
+    template_name = "inquiry.html"
+    form_class = InquiryForm
+    success_url = reverse_lazy('app:inquiry')
+
+    def form_valid(self, form):
+        form.send_email()
+        logger.info('Inquiry sent by {}'.format(form.cleaned_data['name']))
+        return super().form_valid(form)
